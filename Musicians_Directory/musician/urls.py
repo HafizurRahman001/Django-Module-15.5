9776e43dfce22a4d4eb7709e601f27632efb5eb0
hafizur rahman <hafizurrahmanbu@gmail.com>
2023-12-30 23:25:59 +0600

    bug fixing

--- Musicians_Directory/musician/urls.py
+++ Musicians_Directory/musician/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('musician/', views.add_musician, name='add_musician'),
-    path('musician/<int:id>', views.edit_musician, name='edit_musician'),
+    path('musician/<int:id>/', views.edit_musician, name='edit_musician'),
 ]
